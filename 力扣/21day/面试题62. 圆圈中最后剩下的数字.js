c70f1fe845bd60b867aaa8c28ef671db8f1b7b5f
小川 <xiaochuan@xiaochuandeAir.lan>
2020-03-30 11:10:57 +0800

    暂存

---
+++ 力扣/21day/面试题62. 圆圈中最后剩下的数字.js
@@ -0,0 +1,40 @@
+// 面试题62. 圆圈中最后剩下的数字
+// 0,1,,n-1这n个数字排成一个圆圈，从数字0开始，每次从这个圆圈里删除第m个数字。求出这个圆圈里剩下的最后一个数字。
+
+// 例如，0、1、2、3、4这5个数字组成一个圆圈，从数字0开始每次删除第3个数字，则删除的前4个数字依次是2、0、4、1，因此最后剩下的数字是3。
+
+//
+
+// 示例 1：
+
+// 输入: n = 5, m = 3
+// 输出: 3
+// 示例 2：
+
+// 输入: n = 10, m = 17
+// 输出: 2
+//
+
+// 限制：
+
+// 1 <= n <= 10^5
+// 1 <= m <= 10^6
+
+/**
+ * @param {number} n
+ * @param {number} m
+ * @return {number}
+ */
+var lastRemaining = function(n, m) {
+  const arr = [];
+  for (let i = 0; i < n; i++) arr.push(i);
+  while (arr.length !== 1) {
+      
+  }
+//   for (let i = 1; i < n; i++) {
+//     console.log(arr[i * m - 1]);
+//     // arr.splice
+//   }
+};
+
+console.log(lastRemaining((n = 5), (m = 3)));
